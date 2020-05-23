438f2b27e7fd07c6f4c7a82a3c6e9470faf081a4
Yujiro Sato <yujiro.s.work@gmail.com>
2020-05-24 04:23:47 +0900

    has_many :~, db:rails db:migrate:resetデータベースリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_160019) do
+ActiveRecord::Schema.define(version: 2020_05_22_190105) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
